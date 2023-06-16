eb8b0ca2f4d21c5bde820aa238634e6dfb261027
Diego Brito <diego.brito@f1rst.com.br>
2023-06-16 20:52:37 -0300

    adc categoria e itens

--- src/main/java/br/com/fiap/lanchonete/dominio/entidades/Item.java
+++ src/main/java/br/com/fiap/lanchonete/dominio/entidades/Item.java
@@ -1,8 +1,11 @@
 package br.com.fiap.lanchonete.dominio.entidades;
 
-public class Lanche {
+import br.com.fiap.lanchonete.dominio.enumerator.Categoria;
+
+public class Item {
     private int id;
     private String nome;
     private String descricao;
     private String preco;
+    private Categoria categoria;
 }
