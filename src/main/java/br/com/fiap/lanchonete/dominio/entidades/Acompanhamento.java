557b80a5b9a9468e8f40e4152713e26c14b6d1c2
Diego Brito <diego.brito@f1rst.com.br>
2023-06-16 20:53:05 -0300

    Remocao de bebida e acompanhamento

--- src/main/java/br/com/fiap/lanchonete/dominio/entidades/Acompanhamento.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.lanchonete.dominio.entidades;
-
-public class Acompanhamento {
-    private int id;
-    private String nome;
-    private String descricao;
-    private String preco;
-}
--- src/main/java/br/com/fiap/lanchonete/dominio/entidades/Bebida.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.lanchonete.dominio.entidades;
-
-public class Bebida {
-    private int id;
-    private String nome;
-    private String descricao;
-    private String preco;
-}
