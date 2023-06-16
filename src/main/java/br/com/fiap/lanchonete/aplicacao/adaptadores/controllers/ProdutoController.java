78dada1fb5e77dbbfe4f78012aed49af0159277a
Diego Brito <diego.brito@f1rst.com.br>
2023-06-15 23:29:16 -0300

    adc controller com metodos bases propostos

---
+++ src/main/java/br/com/fiap/lanchonete/aplicacao/adaptadores/controllers/ProdutoController.java
@@ -0,0 +1,27 @@
+package br.com.fiap.lanchonete.aplicacao.adaptadores.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ProdutoController {
+
+    @GetMapping
+    public void listarPorCategoria(){
+
+    }
+
+    @PostMapping
+    public void criar(){
+
+    }
+
+    @PutMapping
+    public void editar(){
+
+    }
+
+    @DeleteMapping
+    public void remover(){
+
+    }
+}
