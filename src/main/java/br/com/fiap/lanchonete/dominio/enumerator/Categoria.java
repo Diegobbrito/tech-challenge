1854b36f894e156ea77be35fc8478dd277598c20
Diego Brito <diego.brito@f1rst.com.br>
2023-06-23 20:12:17 -0300

    Atualização do enum de categoria

--- src/main/java/br/com/fiap/lanchonete/dominio/enumerator/Categoria.java
+++ src/main/java/br/com/fiap/lanchonete/dominio/enumerator/Categoria.java
@@ -1,8 +1,29 @@
 package br.com.fiap.lanchonete.dominio.enumerator;
 
 public enum Categoria {
-    BEBIDA,
-    LANCHE,
-    ACOMPANHAMENTO,
-    SOBREMESA;
+    BEBIDA(1, "Bebida"),
+    LANCHE(2, "Lanche"),
+    ACOMPANHAMENTO(3, "Acompanhamento"),
+    SOBREMESA(4, "Sobremesa");
+
+    private int id;
+    private String tipo;
+
+    Categoria(int id, String tipo) {
+        this.id = id;
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Categoria from(final int id) {
+        for (final Categoria element : Categoria.values()) {
+            if (element.getId() == id) {
+                return element;
+            }
+        }
+        return null;
+    }
 }
