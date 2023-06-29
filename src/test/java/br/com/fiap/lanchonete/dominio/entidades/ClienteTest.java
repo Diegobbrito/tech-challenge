c4d435e5716c907af14e35f35c5263f0178a8577
Diego Brito <diego.brito@f1rst.com.br>
2023-06-29 20:48:08 -0300

    Adc pasta de testes

---
+++ src/test/java/br/com/fiap/lanchonete/dominio/entidades/ClienteTest.java
@@ -0,0 +1,14 @@
+package br.com.fiap.lanchonete.dominio.entidades;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ClienteTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
