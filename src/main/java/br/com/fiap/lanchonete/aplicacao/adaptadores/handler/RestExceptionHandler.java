4915fc9cb73f1bb6509f643d7f8f6443d7eb4ddd
Diego Brito <diego.brito@f1rst.com.br>
2023-07-01 00:43:00 -0300

    Update no handler de exception

--- src/main/java/br/com/fiap/lanchonete/aplicacao/adaptadores/handler/RestExceptionHandler.java
+++ src/main/java/br/com/fiap/lanchonete/aplicacao/adaptadores/handler/RestExceptionHandler.java
@@ -1,17 +1,31 @@
 package br.com.fiap.lanchonete.aplicacao.adaptadores.handler;
 
 import br.com.fiap.lanchonete.infraestrutura.adaptadores.exceptions.ClienteInexistenteException;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class RestExceptionHandler {
 
     @ExceptionHandler
-    public ResponseEntity handlerClienteInexistenteException(ClienteInexistenteException ex){
-        return  ResponseEntity.badRequest().build();
+    public ResponseEntity<ExceptionDetails> handlerClienteInexistenteException(ClienteInexistenteException ex){
+        final var details = new ExceptionDetails(ex.getMessage());
+        return new ResponseEntity(details, HttpStatus.NOT_FOUND);
+    }
+}
+
+@Getter
+class ExceptionDetails {
+    private String error;
+    private LocalDateTime timestamp;
+
+    public ExceptionDetails( String details) {
+        this.error = details;
+        this.timestamp = LocalDateTime.now();
     }
 }
