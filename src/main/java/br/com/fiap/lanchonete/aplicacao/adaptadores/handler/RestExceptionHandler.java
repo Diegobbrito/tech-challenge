6d5ed38f6ee3ef1c7bcc54bc54221dd88d522804
Diego Brito <diego.brito@f1rst.com.br>
2023-07-04 23:55:29 -0300

    Adc msg de validação de exceptions

--- src/main/java/br/com/fiap/lanchonete/aplicacao/adaptadores/handler/RestExceptionHandler.java
+++ src/main/java/br/com/fiap/lanchonete/aplicacao/adaptadores/handler/RestExceptionHandler.java
@@ -1,6 +1,9 @@
 package br.com.fiap.lanchonete.aplicacao.adaptadores.handler;
 
 import br.com.fiap.lanchonete.infraestrutura.adaptadores.exceptions.ClienteInexistenteException;
+import br.com.fiap.lanchonete.infraestrutura.adaptadores.exceptions.CpfInvalidoException;
+import br.com.fiap.lanchonete.infraestrutura.adaptadores.exceptions.EmailInvalidoException;
+import br.com.fiap.lanchonete.infraestrutura.adaptadores.exceptions.PedidoInexistenteException;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,24 @@ public class RestExceptionHandler {
     public ResponseEntity<ExceptionDetails> handlerClienteInexistenteException(ClienteInexistenteException ex){
         final var details = new ExceptionDetails(ex.getMessage());
         return new ResponseEntity(details, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionDetails> handlerPedidoInexistenteException(PedidoInexistenteException ex){
+        final var details = new ExceptionDetails(ex.getMessage());
+        return new ResponseEntity(details, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionDetails> handlerCpfInvalidoException(CpfInvalidoException ex){
+        final var details = new ExceptionDetails(ex.getMessage());
+        return new ResponseEntity(details, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionDetails> handlerEmailInvalidoException(EmailInvalidoException ex){
+        final var details = new ExceptionDetails(ex.getMessage());
+        return new ResponseEntity(details, HttpStatus.BAD_REQUEST);
     }
 }
 
