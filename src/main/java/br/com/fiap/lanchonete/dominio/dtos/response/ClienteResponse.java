823fa3fa6c963bba56ecd4c1722d56d6c2083026
Diego Brito <diego.brito@f1rst.com.br>
2023-07-08 17:52:28 -0300

    Atualização da documentação

--- src/main/java/br/com/fiap/lanchonete/dominio/dtos/response/ClienteResponse.java
+++ src/main/java/br/com/fiap/lanchonete/dominio/dtos/response/ClienteResponse.java
@@ -1,16 +1,26 @@
 package br.com.fiap.lanchonete.dominio.dtos.response;
 
 import br.com.fiap.lanchonete.dominio.models.Cliente;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public class ClienteResponse {
-
+    @Schema(example = "1")
+    private int id;
+    @Schema(example = "05506902042")
     private String cpf;
+    @Schema(example = "Diego")
     private String nome;
+    @Schema(example = "diego@teste.com")
     private String email;
     public ClienteResponse(Cliente cliente) {
+        this.id = cliente.getId();
         this.cpf = cliente.getCpf().getValor();
         this.nome = cliente.getNome();
         this.email = cliente.getEmail().getValor();
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getCpf() {
