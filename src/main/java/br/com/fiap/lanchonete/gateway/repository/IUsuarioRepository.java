04f22f53434aa53ce2232e5f2de5b24c1ada117a
Diego Brito <dibbrito@gmail.com>
2023-09-01 12:03:02 -0300

    remoção de lib de segurança

--- src/main/java/br/com/fiap/lanchonete/gateway/repository/IUsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.lanchonete.gateway.repository;
-
-import br.com.fiap.lanchonete.gateway.repository.usuario.UsuarioEntity;
-
-import java.util.Optional;
-
-public interface IUsuarioRepository {
-    Optional<UsuarioEntity> buscarUsuario(String usuario);
-}
--- src/main/java/br/com/fiap/lanchonete/gateway/repository/usuario/JpaUsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.lanchonete.gateway.repository.usuario;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface JpaUsuarioRepository extends JpaRepository<UsuarioEntity, Integer> {
-    Optional<UsuarioEntity> findByNome(String usuario);
-}
