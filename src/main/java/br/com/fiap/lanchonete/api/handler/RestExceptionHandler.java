8d0d90ae205364a53d8a44c205916170b519b2ef
Diego Brito <dibbrito@gmail.com>
2023-09-01 12:04:02 -0300

    atualização do handler com um default

--- src/main/java/br/com/fiap/lanchonete/api/handler/RestExceptionHandler.java
+++ src/main/java/br/com/fiap/lanchonete/api/handler/RestExceptionHandler.java
@@ -5,6 +5,7 @@ import br.com.fiap.lanchonete.core.exception.CpfInvalidoException;
 import br.com.fiap.lanchonete.core.exception.EmailInvalidoException;
 import br.com.fiap.lanchonete.core.exception.PedidoInexistenteException;
 import lombok.Getter;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -37,6 +38,17 @@ public class RestExceptionHandler {
     public ResponseEntity<ExceptionDetails> handlerEmailInvalidoException(EmailInvalidoException ex){
         final var details = new ExceptionDetails(ex.getMessage());
         return new ResponseEntity(details, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionDetails> handlerDataIntegrityViolationException(DataIntegrityViolationException ex){
+        final var details = new ExceptionDetails(ex.getMessage());
+        return new ResponseEntity(details, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler
+    public ResponseEntity<ExceptionDetails> handlerException(Exception ex){
+        final var details = new ExceptionDetails(ex.getMessage());
+        return new ResponseEntity(details, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
 
