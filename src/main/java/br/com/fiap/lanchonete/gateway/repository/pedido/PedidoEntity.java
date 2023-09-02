e20e2547335a1f0bd78e02d1a5234f7a5ab3f022
Diego Brito <dibbrito@gmail.com>
2023-09-02 19:18:15 -0300

    update local

--- src/main/java/br/com/fiap/lanchonete/gateway/repository/pedido/PedidoEntity.java
+++ src/main/java/br/com/fiap/lanchonete/gateway/repository/pedido/PedidoEntity.java
@@ -3,9 +3,9 @@ package br.com.fiap.lanchonete.gateway.repository.pedido;
 import br.com.fiap.lanchonete.core.entity.Pedido;
 import br.com.fiap.lanchonete.core.enumerator.StatusEnum;
 import br.com.fiap.lanchonete.gateway.repository.cliente.ClienteEntity;
+import br.com.fiap.lanchonete.gateway.repository.produto.ProdutoEntity;
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
@@ -19,7 +19,6 @@ import java.util.stream.Collectors;
 @Entity
 @Table(name = "pedidos")
 @NoArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class PedidoEntity {
 
     @Id
@@ -48,11 +47,8 @@ public class PedidoEntity {
         this.valor = pedido.getValor();
         this.status = new StatusPedidoEntity(StatusEnum.from(pedido.getStatus().getId()));
         this.dataCriacao = LocalDateTime.now();
-        this.produtos = pedido.getProdutos().stream().map(p ->{
-            final var produtoPedido =  new ProdutoPedidoEntity();
-            produtoPedido.setQuantidade(p.getQuantidade());
-            return produtoPedido;
-        }
+        this.produtos = pedido.getProdutos().stream().map(p ->
+                new ProdutoPedidoEntity(this, new ProdutoEntity(p.getProduto()), p.getQuantidade())
         ).collect(Collectors.toList());
     }
 
