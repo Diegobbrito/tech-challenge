d1ee33b34d03f52ac4c6070f69b4134a56d710f3
Diego Brito <dibbrito@gmail.com>
2023-09-02 14:14:10 -0300

    rollback

--- src/main/java/br/com/fiap/lanchonete/gateway/repository/pedido/StatusPedidoEntity.java
+++ src/main/java/br/com/fiap/lanchonete/gateway/repository/pedido/StatusPedidoEntity.java
@@ -1,30 +1,30 @@
-//package br.com.fiap.lanchonete.gateway.repository.pedido;
-//
-//import br.com.fiap.lanchonete.core.enumerator.StatusEnum;
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//import java.util.List;
-//
-//@Entity
-//@Table(name = "status_pedido")
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//public class StatusPedidoEntity {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private int id;
-//    private String titulo;
-//
-//    @OneToMany(mappedBy = "status")
-//    private List<PedidoEntity> pedidos;
-//
-//    public StatusPedidoEntity(StatusEnum status) {
-//        this.id = status.getId();
-//        this.titulo = status.getTipo();
-//    }
-//}
+package br.com.fiap.lanchonete.gateway.repository.pedido;
+
+import br.com.fiap.lanchonete.core.enumerator.StatusEnum;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "status_pedido")
+@Getter
+@Setter
+@NoArgsConstructor
+public class StatusPedidoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String titulo;
+
+    @OneToMany(mappedBy = "status")
+    private List<PedidoEntity> pedidos;
+
+    public StatusPedidoEntity(StatusEnum status) {
+        this.id = status.getId();
+        this.titulo = status.getTipo();
+    }
+}
