81e0f3f9eaece05af9be50c5890bfd1929464209
Diego Brito <dibbrito@gmail.com>
2023-09-02 22:00:34 -0300

    update para k8s

--- db/schema.sql
+++ db/schema.sql
@@ -103,5 +103,5 @@ INSERT INTO status_pedido (titulo) VALUES ("Em preparação");
 INSERT INTO status_pedido (titulo) VALUES ("Pronto");
 INSERT INTO status_pedido (titulo) VALUES ("Finalizado");
 
-GRANT ALL PRIVILEGES ON *.* TO 'usuario' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'usuario'@'%' WITH GRANT OPTION;
 flush privileges;
