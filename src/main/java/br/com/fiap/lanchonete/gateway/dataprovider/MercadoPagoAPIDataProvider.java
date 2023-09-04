e8051ac329d3669bc1017f40eba0e8b83b3139a6
Diego Brito <diego.brito@f1rst.com.br>
2023-09-04 18:16:54 -0300

    atualização do pagamento

--- src/main/java/br/com/fiap/lanchonete/gateway/dataprovider/MercadoPagoAPIDataProvider.java
+++ src/main/java/br/com/fiap/lanchonete/gateway/dataprovider/MercadoPagoAPIDataProvider.java
@@ -2,8 +2,7 @@ package br.com.fiap.lanchonete.gateway.dataprovider;
 
 import br.com.fiap.lanchonete.core.entity.Pedido;
 
-
-public class MercadoPagoAPIDataProvider implements IPagamentoDataProvider {
+public class MercadoPagoAPIDataProvider{
 
     public String criarPagamento(Pedido entity) {
         return "00020101021243650016COM.MERCADOLIBRE02013063638f1192a-5fd1-4180-a180-8bcae3556bc35204000053039865802BR5925IZABEL AAAA DE MELO6007BARUERI62070503***63040B6D";
